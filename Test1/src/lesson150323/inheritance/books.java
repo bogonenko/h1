1d98f822125f69c223d93fb9065731660b453e34
Lena <bogonenko@itcwin.com>
2015-03-24 22:22:21 +0400

    solitaire

---
+++ Test1/src/lesson150323/inheritance/books.java
@@ -0,0 +1,10 @@
+package lesson150323.inheritance;
+
+public class books {
+
+}
+
+//http://web.engr.oregonstate.edu/~budd/Books/under/info/solitaire/
+//http://www.cs.umb.edu/~laur/Java/Solitaire/Solitaire.java
+
+//http://www.atilim.edu.tr/~karakaya/oop/
